a0326ba50f44dedb8277c0d14451bf22b9c690b3
Caden McArthur <cadenamcarthur@bethelks.edu>
2025-02-18 22:10:57 -0600

    move peices about the board

--- chessjs/src/components/Box.tsx
+++ chessjs/src/components/Box.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Box.css'
 //this will hold a box to hold the pieces, chess board is 8x8
 //need some sort of event handler to detect if this component is clicked as well, could just be a simple button
 
@@ -6,17 +7,20 @@ interface BoxProps {
     peice: string;
     team: string;
     canMoveTo: boolean; //dont know what else we may need here
+    onClick: () => void;
+    isSelected: boolean;
 }
 const Box: React.FC<BoxProps> = ({
     peice,
     team,
-    canMoveTo
+    canMoveTo,
+    onClick,
+    isSelected
 
 }) => {
 
-    console.log(peice, team, canMoveTo);
   return (
-    <div className='box'>
+    <div className={`box ${isSelected ? 'highlight' : ''}`} onClick={() => { onClick(); }}>
       {peice}
     </div>
   )
--- chessjs/src/pages/Home.tsx
+++ chessjs/src/pages/Home.tsx
@@ -1,54 +1,97 @@
-import React from 'react'
-import Box from '../components/Box'
+import React, { useState } from "react";
+import Box from "../components/Box";
+
 const Home = () => {
-    let board = [];
-    // assuming this will change, but for now just a simple 8x8 board
-    for(let i = 0; i < 8; i++){
-        let row = [];
-        for(let j = 0; j < 8; j++){
-            if (j % 2 == 0){
-                row.push(<Box peice="pawn" team="black" canMoveTo={true}/>)
-            }else{
-                row.push(<Box peice="rook" team="white" canMoveTo={true}/>)
-            }
-        }
-        board.push(row);
+  const [selected, setSelected] = useState({
+    peice: "",
+    team: "",
+    row: -1,
+    col: -1,
+  });
+
+  const [testboard, setTestBoard] = useState([
+    ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"],
+    ["pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["wpawn", "wpawn", "wpawn", "wpawn", "wpawn", "wpawn", "wpawn", "wpawn"],
+    [
+      "wrook",
+      "wknight",
+      "wbishop",
+      "wqueen",
+      "wking",
+      "wbishop",
+      "wknight",
+      "wrook",
+    ],
+  ]);
+  const handleOnClick = (
+    peice: string,
+    team: string,
+    row: number,
+    col: number
+  ) => {
+    console.log(peice, team);
+    if (selected.peice == "" && selected.team == "") {
+      setSelected({ peice, team, row, col });
+      return;
+    }else{
+        //handle moving the peice to the new location, dont worry about peice rules at the moment
+        console.log('test move')
+        const newBoard = [...testboard]; //create a copy of the board
+        newBoard[row][col] = selected.peice; //copy the peice to the new board at the selected location
+        newBoard[selected.row][selected.col] = ""; //remove the peice from the old location
+        setTestBoard(newBoard); //update board with new board copy
+        setSelected({ peice: "", team: "", row: -1, col: -1 });
+
     }
+  };
+  //evvery peice needs a unique id probably
+  //highlight a piece when clicked
 
-    //event handlers, check if the clicked box has a peice,
-    //iterate and find valid moves, highlight those boxes
-    //if a valid move is clicked, move the peice to that box
-    //check for checkmate, check for stalemate
-    //check for pawn promotion
-  return (
-    <div className="board">
-        <div className="row">
-            {board[0]}
-        </div>
-        <div className="row">
-            {board[1]}
-        </div>
-        <div className="row">
-            {board[2]}
-        </div>
-        <div className="row">
-            {board[3]}
-        </div>
-        <div className="row">
-            {board[4]}
-        </div>
-        <div className="row">
-            {board[5]}
-        </div>
-        <div className="row">
-            {board[6]}
-        </div>
-        <div className="row">
-            {board[7]}
-        </div>
-    
-    </div>
-  )
-}
+  let board = [];
+  for (let i = 0; i < 8; i++) {
+    let row = [];
+    for (let j = 0; j < 8; j++) {
+      const piece = testboard[i][j];
+      if (piece) {
+        const team = piece.startsWith("w") ? "white" : "black";
+        const pieceType = piece.startsWith("w") ? piece.slice(1) : piece;
+        row.push(
+          <Box
+            key={`${i}-${j}`}
+            peice={pieceType}
+            team={team}
+            canMoveTo={true}
+            onClick={() => handleOnClick(pieceType, team, i, j)}
+            isSelected={selected.row === i && selected.col === j}
+          />
+        );
+        console.log(i, j);
+      } else {
+        row.push(
+          <Box
+            key={`${i}-${j}`}
+            peice=""
+            team=""
+            canMoveTo={true}
+            onClick={() => handleOnClick("", "", i, j)}
+            isSelected={selected.row === i && selected.col === j}
+          />
+        );
+      }
+    }
+    board.push(
+      <div key={i} className="row">
+        {row}
+      </div>
+    );
+  }
 
-export default Home
+  return <div className="board">{board}</div>;
+};
+
+export default Home;
